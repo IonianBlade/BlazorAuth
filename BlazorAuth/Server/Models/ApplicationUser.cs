186dc66d4984741c004151b23e85ba2227419dcd
Ionian Blade <vipmen5555@gmail.com>
2022-12-22 17:11:14 +0300

    Добавьте файлы проекта.

---
+++ BlazorAuth/Server/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using BlazorAuth.Shared;
+using Microsoft.AspNetCore.Identity;
+
+namespace BlazorAuth.Server.Models
+{
+	public class ApplicationUser : IdentityUser
+	{
+		public List<SuperHero> SuperHeroes { get; set; } = new List<SuperHero>();
+	}
+}
